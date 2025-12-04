5bffd117c264cc1b0fb1695480d420426ee73095
alicialisal <ajuanita@student.ciputra.ac.id>
2025-12-04 14:03:27 +0800

    bottom sheet done

--- Assets/LiquidGlass/BottomSheetController.cs
+++ Assets/LiquidGlass/BottomSheetController.cs
@@ -8,6 +8,8 @@ public class BottomSheetController : MonoBehaviour, IDragHandler, IEndDragHandle
     public float collapsedY = -1348f;
     public float expandedY = 0f;
     public float smoothSpeed = 20f;
+    public float dragThreshold = 15f; // geser sedikit saja langsung naik
+    private float startDragY;
 
     // private float collapsedY;
     private float targetY;
@@ -28,23 +30,40 @@ public class BottomSheetController : MonoBehaviour, IDragHandler, IEndDragHandle
 
     public void OnDrag(PointerEventData eventData)
     {
-        float drag = eventData.delta.y * 1.8f; // buat sensitif
-        float newY = sheet.anchoredPosition.y + drag;
+        // float drag = eventData.delta.y * 1.8f; // buat sensitif
+        // float newY = sheet.anchoredPosition.y + drag;
 
-        sheet.anchoredPosition = new Vector2(
-            0,
-            Mathf.Clamp(newY, collapsedY, expandedY)
-        );
+        // sheet.anchoredPosition = new Vector2(
+        //     0,
+        //     Mathf.Clamp(newY, collapsedY, expandedY)
+        // );
+
+        if (eventData.delta.y > 0)
+        {
+            // drag ke atas → langsung expand
+            if (Mathf.Abs(eventData.delta.y) > dragThreshold)
+                targetY = expandedY;
+        }
+        else
+        {
+            // drag ke bawah → collapse
+            if (Mathf.Abs(eventData.delta.y) > dragThreshold)
+                targetY = collapsedY;
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        float mid = (collapsedY + expandedY) / 2f;
+        // float mid = (collapsedY + expandedY) / 2f;
 
-        if (sheet.anchoredPosition.y > mid)
-            targetY = expandedY;
-        else
-            targetY = collapsedY;
+        // if (sheet.anchoredPosition.y > mid)
+        //     targetY = expandedY;
+        // else
+        //     targetY = collapsedY;
+
+         // Fallback jika tidak kena threshold (optional)
+        float mid = (collapsedY + expandedY) / 2f;
+        targetY = (sheet.anchoredPosition.y > mid) ? expandedY : collapsedY;
     }
 
     public void ShowCollapsed() => targetY = collapsedY;
